e86e0143c35eddb5c1182c9b9cd1928cb9852d51
parkeeseul <eeseul.p@gmail.com>
2021-05-13 14:38:38 +0900

    feat: @Query, 리포지토리 메소드에 쿼리 정의하기

--- src/main/java/com/seul/jpa/study/datajpa/repository/MemberRepository.java
+++ src/main/java/com/seul/jpa/study/datajpa/repository/MemberRepository.java
@@ -13,4 +13,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query(name = "Member.findByUsername") // @Query 가 없어도 Spring jpa 에서 엔티티명.메서드 로 네임드 쿼리를 찾기 때문에 해당 내용을 제거해도 무방하다
     List<Member> findByUsername(@Param("username") String username);
+
+    @Query("SELECT m FROM Member m WHERE m.username = :username and m.age = :age") // 이름 없는 네임드 쿼리라고 볼 수 있다.
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
