bf187629e9531fee065d57919c42d6df404d6edf
parkeeseul <eeseul.p@gmail.com>
2021-05-13 14:12:32 +0900

    feat: 메소드 이름으로 쿼리 생성  - 주로 간단하게 2개 정도의 조건을 가질 떄 사용하면 좋음  - 필드명 변경시 컴파일 에러가 발생하기 때문에 쿼리 실수를 줄일 수 있음

--- src/main/java/com/seul/jpa/study/datajpa/repository/MemberRepository.java
+++ src/main/java/com/seul/jpa/study/datajpa/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.seul.jpa.study.datajpa.repository;
 import com.seul.jpa.study.datajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
