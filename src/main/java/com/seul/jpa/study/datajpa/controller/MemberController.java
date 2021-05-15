06c7963af06b058277317d9cb9e70b7a2f661006
parkeeseul <eeseul.p@gmail.com>
2021-05-15 15:50:36 +0900

    feat: 확장기능 - web 확장(페이징과 정렬)

--- src/main/java/com/seul/jpa/study/datajpa/controller/MemberController.java
+++ src/main/java/com/seul/jpa/study/datajpa/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.seul.jpa.study.datajpa.controller;
 
+import com.seul.jpa.study.datajpa.dto.MemberDto;
 import com.seul.jpa.study.datajpa.entity.Member;
 import com.seul.jpa.study.datajpa.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,11 +29,16 @@ public class MemberController {
         return member.getUsername();
     }
 
-    @PostConstruct
-    public void init() {
-        memberRepository.save(new Member("userA"));
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable)
+                .map(MemberDto::new);
     }
 
-
-
+    @PostConstruct
+    public void init() {
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + 1, i));
+        }
+    }
 }
--- src/main/java/com/seul/jpa/study/datajpa/dto/MemberDto.java
+++ src/main/java/com/seul/jpa/study/datajpa/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package com.seul.jpa.study.datajpa.dto;
 
+import com.seul.jpa.study.datajpa.entity.Member;
 import lombok.Data;
 
 @Data
@@ -13,5 +14,11 @@ public class MemberDto {
         this.id = id;
         this.username = username;
         this.teamName = teamName;
+    }
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
+        this.teamName = null;
     }
 }
