b510c79e7cb574f0be102ab66ea7e9b9d22cc332
parkeeseul <eeseul.p@gmail.com>
2021-05-16 11:22:29 +0900

    feat: 나머지 기능들 - query by example

--- src/test/java/com/seul/jpa/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/seul/jpa/study/datajpa/repository/MemberRepositoryTest.java
@@ -6,9 +6,7 @@ import com.seul.jpa.study.datajpa.entity.Team;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -283,5 +281,34 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findAll(spec);
 
         assertThat(result).hasSize(1);
+    }
+
+    @Test
+    void queryByExample() {
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        // Probe 생성
+        Member member = new Member("m1");
+        Team team = new Team("teamA");
+        member.setTeam(team);
+
+        // ExampleMatcher 생성, age 프로퍼티는 무시
+        ExampleMatcher matcher = ExampleMatcher.matching()
+                .withIgnorePaths("age");
+
+        Example<Member> example = Example.of(member, matcher);
+
+        List<Member> result = memberRepository.findAll(example);
+
+        assertThat(result.get(0).getUsername()).isEqualTo("m1");
     }
 }
